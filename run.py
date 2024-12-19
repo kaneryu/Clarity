fc72bddbe50478624218b5ba757d2aaa808271af
kaneryu <72762358+kaneryu@users.noreply.github.com>
2024-12-19 00:11:04 -0600

    fix(build): Update run.py

--- run.py
+++ run.py
@@ -14,7 +14,7 @@
 # nuitka-project: --copyright="This is free and open-source software -- GNU GPL v3"
 # nuitka-project: --windows-product-name="InnerTuneDesktop"
 
-# nuitka-project: --disable-console
+# disabled: --disable-console
 # nuitka-project: --user-package-configuration-file=./nuitka-fix.config.yml 
 
 import src.universal
