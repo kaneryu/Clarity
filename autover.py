5f9f383320af0775a42cba37d31ef80b2fd571c5
kaneryu <kaneryu.yt@gmail.com>
2024-12-19 22:41:46 -0600

    fix(autover): clean up autover.py for use in commit hooks

--- autover.py
+++ autover.py
@@ -1,6 +1,7 @@
 import sys
 import subprocess
 import re
+
 commit_history = subprocess.check_output(['git', 'log', '--oneline']).decode('utf-8').split('\n')
 commit_count = len(commit_history) - 1
 
@@ -43,35 +44,40 @@ major = 0
 minor = 0
 patch = 0
 
-regex = re.compile(r"(?P<hash>.*?) (?P<type>feat|chore|fix|refactor)(\((?P<scope>.*?)\))?: (?P<desc>.*)")
+regex = re.compile(r"(?P<hash>.*?) ?(?P<type>feat|chore|fix|perf|refactor|docs)(\((?P<scope>.*?)\))?: (?P<desc>.*)")
 
+def getVer():
+    global major, minor, patch
+    for commit in commit_history:
 
-for commit in commit_history:
+        matches = regex.match(commit)
+        if not matches:
+            continue
+        hash = matches.group("hash")
+        type = matches.group("type")
+        scope = matches.group("scope")
+        desc = matches.group("desc")
+        
+        
+        mitem = typeincmap[mapmap[type]]
+        if mitem["inc"] == "major":
+            major += mitem["amt"]
+            minor = 0
+            patch = 0
+        if mitem["inc"] == "minor":
+            minor += mitem["amt"]
+            patch = 0
+        if mitem["inc"] == "patch":
+            patch += mitem["amt"]
+        
+        if "BREAKING CHANGE" in commit:
+            major += 1
 
+    return f"{major}.{minor}.{patch}"
+
+def checkCommit(commit):
     matches = regex.match(commit)
     if not matches:
-        continue
-    hash = matches.group("hash")
-    type = matches.group("type")
-    scope = matches.group("scope")
-    desc = matches.group("desc")
+        return False
+    return True
     
-    
-    mitem = typeincmap[mapmap[type]]
-    if mitem["inc"] == "major":
-        print("found major inc", commit)
-        major += mitem["amt"]
-        minor = 0
-        patch = 0
-    if mitem["inc"] == "minor":
-        print("found minor inc", commit)
-        minor += mitem["amt"]
-        patch = 0
-    if mitem["inc"] == "patch":
-        print("found patch inc", commit)
-        patch += mitem["amt"]
-    
-    if "BREAKING CHANGE" in commit:
-        major += 1
-
-print(f"Major: {major}, Minor: {minor}, Patch: {patch}")
