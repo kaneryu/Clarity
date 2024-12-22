961082b303a92df3e4e06c839d2f16baef04238d
kaneryu <kaneryu.yt@gmail.com>
2024-12-21 23:50:16 -0600

    chore(build): remove anti-bloat from pluginlist

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 # nuitka-project: --mode=standalone
 # The PySide6 plugin covers qt-plugins
 
-# nuitka-project: --enable-plugin=pyside6, anti-bloat
+# nuitka-project: --enable-plugin=pyside6
 # nuitka-project: --include-qt-plugins=sensible,styles,qml
 
 # nuitka-project: --include-data-dir=src/app/qml=src/app/qml
@@ -13,7 +13,7 @@
 # nuitka-project: --copyright="This is free and open-source software -- GNU GPL v3"
 # nuitka-project: --windows-product-name="InnerTuneDesktop"
 
-# nutika-project: --product-version=0.9.0
+# nuitka-project: --product-version=0.9.0
 # nuitka-project: --file-version=0.9.0
 
 # nuitka-project: --output-filename=InnerTuneDesktop
