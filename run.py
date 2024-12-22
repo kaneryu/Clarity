560277990b79aeda445b49270773f9e977fde871
kaneryu <kaneryu.yt@gmail.com>
2024-12-21 23:47:29 -0600

    chore: edit.gitignore

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 # Compilation mode, support OS-specific options
-# nuitka-project: --mode=app
+# nuitka-project: --mode=standalone
 # The PySide6 plugin covers qt-plugins
 
-# nuitka-project: --enable-plugin=pyside6
+# nuitka-project: --enable-plugin=pyside6, anti-bloat
 # nuitka-project: --include-qt-plugins=sensible,styles,qml
 
 # nuitka-project: --include-data-dir=src/app/qml=src/app/qml
@@ -12,6 +12,9 @@
 # nuitka-project: --file-description="InnerTuneDesktop -- A music player."
 # nuitka-project: --copyright="This is free and open-source software -- GNU GPL v3"
 # nuitka-project: --windows-product-name="InnerTuneDesktop"
+
+# nutika-project: --product-version=0.9.0
+# nuitka-project: --file-version=0.9.0
 
 # nuitka-project: --output-filename=InnerTuneDesktop
 # disabled: --disable-console
